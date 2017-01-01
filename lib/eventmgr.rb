6ce74105495c103c9c788c2ea03dd6992a10286e
nmacawile <nmacawile@gmail.com>
2017-01-02 00:23:07 +0800

    day of week and hour targeting completed

--- lib/eventmgr.rb
+++ lib/eventmgr.rb
@@ -1,6 +1,7 @@
 require 'csv'
 require 'sunlight/congress'
 require 'erb'
+require 'date'
 
 Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
 
@@ -37,6 +38,10 @@ def clean_phone_number(raw_number)
 
 end
 
+def string_to_date date_registered
+	DateTime.strptime date_registered, "%m/%d/%Y  %k:%M"
+end
+
 puts "EventManager initialized."
 
 contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :symbol
@@ -44,14 +49,25 @@ contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :sy
 template_letter = File.read "form_letter.erb"
 erb_template = ERB.new template_letter
 
+reg_hours_tally = Hash.new(0)
+reg_days_of_week_tally = Hash.new(0)
+
 contents.each do |row|
   id = row[0]
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
   legislators = legislators_by_zipcode(zipcode)
   phone_number = clean_phone_number(row[:homephone])
-  puts "#{phone_number}"
+  date_registered = string_to_date(row[:regdate])
+  reg_hours_tally[date_registered.hour] += 1
+  reg_days_of_week_tally[Date::DAYNAMES[date_registered.wday]] += 1
+
   form_letter = erb_template.result(binding)
 
   save_thank_you_letters(id,form_letter)
 end
+
+puts "No. of Registrations by Hour: "
+p reg_hours_tally.sort_by { |_,count| count }.reverse.to_h
+puts "No. of Registrations by Day of Week: "
+p reg_days_of_week_tally.sort_by { |_,count| count }.reverse.to_h
